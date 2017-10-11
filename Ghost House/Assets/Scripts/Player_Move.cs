ad7a6a314779ee75d2b6e48fd30fdca4bfcf4a5f
Unknown <sherbetlemon6@gmail.com>
2017-10-11 13:07:36 -0600

    Added controler

--- Ghost House/Assets/Scripts/Player_Move.cs
+++ Ghost House/Assets/Scripts/Player_Move.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Player_Move : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
 
-	// Use this for initialization
-	void Start () {
 	
-	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
 	}
 }
