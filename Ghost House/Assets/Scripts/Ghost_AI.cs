8239747deddc80a8f2d5ac79835861ef8cf1fe0f
Unknown <sherbetlemon6@gmail.com>
2017-10-13 13:12:22 -0600

    Added Ghosts

---
+++ Ghost House/Assets/Scripts/Ghost_AI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost_AI : MonoBehaviour {
+
+	public float moveSpeed;
+	public Transform target;
+	public int damage;
+
+
+	
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
