4958e58dd20f6498fa2205c6f6efbae5c5e4cc32
Unknown <sherbetlemon6@gmail.com>
2017-10-25 13:10:14 -0600

    Added Flash light control and house.

---
+++ Ghost House/Assets/Scripts/Flash_Light.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flash_Light : MonoBehaviour {
+
+	public bool lightOn;
+
+	Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+		//Set light default to on
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Toggle light on/off when L key is pressed.
+		if (Input.GetKeyUp (KeyCode.F) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+		}
+
+		else if (Input.GetKeyUp (KeyCode.F) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+		}
+	
+	}
+}
